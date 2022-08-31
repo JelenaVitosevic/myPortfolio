21a238f63443241d3ca8e11376a20a4fdf1b052d
JelenaVitosevic <vitosevicjelena83997@gmail.com>
2022-08-31 11:14:40 +0200

    added avatar pictures and logo in header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,13 +3,16 @@ import styles from './Header.module.css';
 import { Link } from 'react-router-dom';
 import MenuContainer from '../menuContainer';
 import { menuItems } from '../../data/menuItems';
+import logo from '../images/avatars/logo.png'
 
 function Header() {
   return (
                 <header className={styles.header}>
                     <div className={styles.wrapper}>
                         <div className={styles.logoWrapper}>
-                            <Link to="/" className={styles.logo}>LOGO</Link>
+                            <Link to="/" className={styles.logoWrapper}>
+                                <img src={logo} className={styles.logo}></img>
+                            </Link>
                         </div>
                         <div className={styles.navWrapper}>
                            <MenuContainer items={menuItems}/>
